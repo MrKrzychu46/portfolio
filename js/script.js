8dfbdf7f62edfd00328d5b7a3980deeef65efc7a
Krzysztof Łyszczarz <klyszczarz03@gmail.com>
2025-03-27 15:21:00 +0100

    Naprawa serwera

--- js/script.js
+++ js/script.js
@@ -184,7 +184,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     main.innerHTML = "<h2>Ładowanie artykułu...</h2><p>Proszę czekać...</p>";
 
-    fetch("https://portfolio-3juy.vercel.app/api/summarize", {
+    fetch("https://portfolio-xfkw.vercel.app/api/summarize", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
